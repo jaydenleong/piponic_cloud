901d8b72d7b9709a51a9967e425836a10d6e3b5d
Jayden Leong <jayden.aandkrobotics@gmail.com>
2021-02-04 13:24:30 -0700

    [major] Implement Cloud function to send device updates to Realtime DB

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # piponic_cloud
 Google cloud functions for a aquaponic or hydroponic monitoring and control system. 
+
+## Function Emulation
+
+Install dependencies using Node 12 (make sure you have this version):
+
+```
+cd functions
+npm install
+```
+
+To run cloud functions locally, you can use the Firebase emulator. Please initialise this using the following commands:
+
+```
+firebase init emulators
+firebase emulators:start
+```
+
+## Function Deploy
+
+Run the following to deploy cloud functions to Firebase:
+
+```
+sudo firebase deploy --only functions
+```
+
+Or, to deploy only a single function:
+
+```
+sudo firebase deploy --only "functions:<FUNCTION-NAME>"
+```
+
+Sometimes, eslint errors will prevent deployment. To fix linter errors automatically, try the following:
+
+```
+npm run lint -- --fix
+```
+
+
--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,46 @@
-// Import NPM Packages
-// const admin = require('firebase-admin');
+/*
+ * File: index.js
+ *
+ * Purpose: Firebase functions that connect
+ *          IoT devices to the piponics mobile
+ *          application
+ *
+ * Author: Jayden Leong <jdleong58@gmail.com>
+ *
+ * Date: February 4, 2021
+ */
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const iot = require("@google-cloud/iot");
+
+admin.initializeApp();
+
+// Reference to root of real-time database in Firebase
+const database = admin.database();
 
 // Client to interface with Google Cloud IoT devices
 const client = new iot.v1.DeviceManagerClient();
 
-// Name of Google Cloud (and Firebase) Project
+// Configuration variables
 const GCLOUD_PROJECT = "piponics";
 const REGISTRY_ID = "RaspberryPis";
 const IOT_REGION = "us-central1";
+const IOT_TOPIC = "device-events";
 
-// start cloud function
-exports.configUpdate = functions.firestore
-    // assumes a document whose ID is the same as the deviceid
-    .document("RaspberryPis/{deviceId}")
+/*
+ * Function: iotDeviceConfigUpdate
+ *
+ * Listens to updates from Firestore collection,
+ * then updates Google Cloud IoT device configurations.
+ *
+ * Assumes the IoT device registry name (REGISTRY_ID)
+ * is the same as the Firestore collection,
+ * and the IoT device name is the same as the
+ * document ID. Visit Firestore console to see
+ * database structure.
+ */
+exports.iotDeviceConfigUpdate = functions.firestore
+    .document(REGISTRY_ID.concat("/{deviceId}"))
     .onWrite((change, context) => {
       if (context) {
         console.log("Updating device: ", context.params.deviceId);
@@ -27,8 +53,37 @@ exports.configUpdate = functions.firestore
       }
     });
 
+/*
+ * Function: iotStoreDeviceUpdates
+ *
+ * Subscribes to IoT device updates in Google Pub-Sub,
+ * then saves this to Firebase real-time database.
+ * The mobile app pulls device status from this
+ * real-time database.
+ *
+ * Assumes the IoT devices publish to IOT_TOPIC
+ *
+ * NOTE: this function does not work in emulator.
+ *       This is for unknown reasons. Please deploy
+ *       to test
+ */
+exports.iotStoreDeviceUpdates = functions.pubsub
+    .topic(IOT_TOPIC)
+    .onPublish((message) => {
+      // Fetch data from PubSub topic
+      const deviceId = message.attributes.deviceId;
+      const deviceData = message.json;
+      console.log("Recieved msg from: ", deviceId);
+      console.log("Msg data: ", deviceData);
+
+      // Write data to Firebase real-time db
+      database.ref(deviceId).set(deviceData);
+
+      return;
+    });
+
 /**
- * Function to generate request to change Google Cloud IoT Device config
+ * Generate request to change Google Cloud IoT Device config
  *
  * @param {string} deviceId Google Cloud IoT Device name
  * @param {object} configData the configuration data to send
@@ -47,3 +102,5 @@ function generateRequest(deviceId, configData) {
     binaryData: dataValue,
   };
 }
+
+
