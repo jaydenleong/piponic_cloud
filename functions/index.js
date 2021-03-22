6135e1b71128ce4b0e33b5fcd175bf168289f687
Jayden Leong <jayden.aandkrobotics@gmail.com>
2021-03-22 12:58:53 -0600

    [minor] Working prototype of error data and default config

--- functions/index.js
+++ functions/index.js
@@ -31,6 +31,32 @@ const IOT_TOPIC = "device-events";
 const STATUS_COLLECTION = "Status";
 const HISTORY_COLLECTION = "History";
 const CONFIG_COLLECTION = "Config";
+const ERROR_COLLECTION = "Error";
+
+// Notification Messages
+const GENERIC_WARN_MSG = "Please check your system to avoid damage...";
+const TEMP_HIGH_MSG = "WARNING: TEMPERATURE TOO HIGH...";
+const TEMP_LOW_MSG = "WARNING: TEMPERATURE TOO LOW...";
+
+// Default Firestore database documents for Raspberry Pis
+const DEFAULT_CONFIG_DOC = { // Default system settings
+  max_ph: 10,
+  max_temperature: 15,
+  min_ph: 5,
+  min_temperature: 25,
+  peristaltic_pump_on: false,
+  target_ph: 7,
+};
+
+const DEFAULT_ERROR_DOC = { // Default there are no errors
+  PH_HIGH: false,
+  PH_LOW: false,
+  TEMP_HIGH: false,
+  TEMP_LOW: false,
+  WATER_LEVEL_LOW: false,
+  BATTERY_LOW: false,
+  LEAK_DETECTED: false,
+};
 
 /*
  * Function: iotDeviceConfigUpdate
@@ -57,6 +83,29 @@ exports.iotDeviceConfigUpdate = functions.firestore
         throw (Error("no context from trigger"));
       }
     });
+
+/**
+ * Gets system error information from Firestore. Error data includes
+ * pH values out of range, battery level low, etc.
+ *
+ * @param {string} deviceId Raspberry Pi's Google Cloud IoT Device name
+ *
+ * @return {object} The error data for a device
+ */
+/*
+async function getErrorDataForDevice(deviceId) {
+  let errorData = null;
+
+  await firestore.collection(ERROR_COLLECTION)
+      .doc(deviceId).get().then((documentSnapshot) => {
+        if (documentSnapshot.exists) {
+          errorData = documentSnapshot.data();
+        }
+      });
+
+  return errorData;
+}
+*/
 
 /*
  * Function: iotStoreDeviceUpdates
@@ -89,7 +138,7 @@ exports.iotStoreDeviceUpdates = functions.pubsub
       database.ref(deviceId).set(deviceData);
 
       // Update sensor measurements in Firestore
-      firestore.collection(STATUS_COLLECTION).doc(deviceId).update(deviceData);
+      firestore.collection(STATUS_COLLECTION).doc(deviceId).set(deviceData);
 
       // Push the new message into Firestore using the Firebase Admin SDK.
       firestore.collection(HISTORY_COLLECTION)
@@ -97,37 +146,128 @@ exports.iotStoreDeviceUpdates = functions.pubsub
           .collection(HISTORY_COLLECTION)
           .add(deviceData);
 
-      // Create a notification demo if the leak sensor is above a certain value
-      // TODO(Jayden) Fetch thresholds from database,
-      // programmable by user in notification settings.
-      // (deviceId == "CarsonPi" || deviceId == "LynesPi"))
-      /*
-      if (deviceData.leak > 1.5 && deviceId == "LynesPi") {
-        console.log("Leak Detected on System: ", deviceId);
+      // Create a notification if sensor thresholds are surpassed
+      return firestore.collection(CONFIG_COLLECTION)
+          .doc(deviceId)
+          .get().then((documentSnapshot) => {
+            let piConfigData;
 
-        const payload = {
-          notification: {
-            title: "WARNING: LEAK DETECTED",
-            body: "Please check your system to avoid damage...",
-            sound: "default",
-          },
-        };
+            // Create default configuration settings if none are found
+            if (!documentSnapshot.exists) {
+              console.log("[WARN] No configuaration status exists for: ",
+                  deviceId, "creating default...");
+              piConfigData = DEFAULT_CONFIG_DOC;
+              firestore.collection(CONFIG_COLLECTION)
+                  .doc(deviceId)
+                  .set(piConfigData);
+            } else { // Get configuration from Firestore if found
+              piConfigData = documentSnapshot.data();
+            }
 
-        //  Create an options object that contains the time to live
-        //    for the notification and the priority
-        const options = {
-          priority: "high",
-          timeToLive: 60 * 60 * 24,
-        };
+            console.log("Config Data finish");
+            console.log(piConfigData);
 
-        // TODO(Jayden) Keep topic as constant
-        return admin.messaging().sendToTopic("leakNotifications",
-            payload,
-            options);
-      }
-      */
+            // Get Error data for system
+            firestore.collection(ERROR_COLLECTION)
+                .doc(deviceId).get().then((documentSnapshot) => {
+                  let piErrorData;
+
+                  // Add default error data if none available
+                  if (!documentSnapshot.exists) {
+                    console.log("[WARN] No error settings exist for: ",
+                        deviceId,
+                        ", creating default...");
+
+                    // Create default error settings
+                    piErrorData = DEFAULT_ERROR_DOC;
+                    firestore.collection(ERROR_COLLECTION)
+                        .doc(deviceId)
+                        .set(DEFAULT_ERROR_DOC);
+                    return;
+                  } else { // Fetch error data from Firebase Error collection
+                    piErrorData = documentSnapshot.data();
+                  }
+
+                  console.log("Error Data finish");
+                  console.log(piErrorData);
+
+                  const options = {
+                    priority: "high",
+                    timeToLive: 60 * 60 * 24,
+                  };
+
+                  // TODO(Jayden): Get latest status value also
+
+                  for (const [key, val] of Object.entries(piConfigData)) {
+                  // Check that
+                    switch (key) {
+                      case "max_temperature":
+                        if ("temperature" in deviceData) {
+                          if (deviceData.temperature > val) {
+                            const notificationMessage = {
+                              notification: {
+                                title: TEMP_HIGH_MSG,
+                                body: GENERIC_WARN_MSG,
+                                sound: "default",
+                              },
+                            };
+
+                            // Send the notification
+                            admin.messaging()
+                                .sendToTopic(
+                                    deviceId,
+                                    notificationMessage,
+                                    options);
+                          }
+                        }
+                        break;
+                      case "min_temperature":
+                        console.log("Min temp");
+                        if ("temperature" in deviceData) {
+                          if (deviceData.temperature < val) {
+                            console.log("Temp too low");
+                            const notificationMessage = {
+                              notification: {
+                                title: TEMP_LOW_MSG,
+                                body: GENERIC_WARN_MSG,
+                                sound: "default",
+                              },
+                            };
+
+                            // Send the notification
+                            admin.messaging()
+                                .sendToTopic(
+                                    deviceId,
+                                    notificationMessage,
+                                    options);
+                          }
+                        }
+                        break;
+                      case "max_ph":
+                        break;
+                      case "min_ph":
+                        break;
+                      default:
+                        break;
+                    }
+                  }
+                });
+          });
     });
 
+/**
+ * Generate request to change Google Cloud IoT Device config
+ *
+ * @param {string} deviceId Google Cloud IoT Device name
+ * @param {object} configData the configuration data to send
+ *
+ * @return {object} the formatted request to Google Cloud IoT
+ */
+/*
+function detectSystemErrors(deviceId, configData) {
+
+}
+*/
 /**
  * Generate request to change Google Cloud IoT Device config
  *
